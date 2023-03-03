4dfedca04274ba6440c79b98cc1a454d682b485a
gitahievans <gitahievance@gmail.com>
2023-03-03 22:38:50 +0300

    Final styling

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
 const icon = document.querySelector(".hamburger-icon");
 const menu = document.querySelector(".menu");
-const tri = document.querySelector(".tri");
 const tText = document.querySelector(".transform");
 
-[icon, tri].forEach((ele) => {
-  ele.addEventListener("click", () => {
+icon.addEventListener("click", () => {
     menu.classList.toggle("active");
   });
-});
-
-
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.toggle("show");
-//     }
-//   });
-// });
-
-// tText.forEach((el) => {
-//   observer.observe(el);
-// });
